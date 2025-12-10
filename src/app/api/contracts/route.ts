1ffdac5a9263bbca203bdfca5ae1812f80ac6deb
ronniepettersonn <petterson28@hotmail.com>
2025-12-10 09:49:51 -0300

    update security vercel

--- src/app/api/contracts/route.ts
+++ src/app/api/contracts/route.ts
@@ -1,4 +1,5 @@
 // src/app/api/contracts/route.ts
+export const runtime = 'nodejs'
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,25 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+// truque pra não recriar client em dev (Next, etc.)
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
 }
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['error', 'warn'],
+    adapter,
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
